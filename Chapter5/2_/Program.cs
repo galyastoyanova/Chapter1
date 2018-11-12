c525ed400eb818db8aa4d2e9ec74ad4eb78e0dd6
galyastoyanova <43786376+galyastoyanova@users.noreply.github.com>
2018-11-12 13:38:47 +0200

    Add files via upload

---
+++ Chapter5/2_/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        
+        {
+            Console.Write("Enter first number: ");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            int b = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter third number: ");
+            int c = Int32.Parse(Console.ReadLine());
+
+            if (a < 0 && b < 0 && c < 0) Console.WriteLine("-");
+            else if (a >= 0 && b >= 0 && c >= 0) Console.WriteLine("+");
+            else if (a < 0 && b < 0 && c >= 0) Console.WriteLine("+");
+            else if (a < 0 && b >= 0 && c < 0) Console.WriteLine("+");
+            else if (a >= 0 && b < 0 && c < 0) Console.WriteLine("+");
+            else if (a < 0 && b >= 0 && c >= 0) Console.WriteLine("-");
+            else if (a >= 0 && b < 0 && c >= 0) Console.WriteLine("-");
+            else if (a >= 0 && b >= 0 && c < 0) Console.WriteLine("-");
+        }
+    }
+}
